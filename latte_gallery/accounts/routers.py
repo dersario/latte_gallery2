fdb46f2e897394acaaf489b2e0bf2d6afab919c1
dersar <vanya20061482@gmail.com>
2024-12-22 21:07:41 +0400

    load

---
+++ latte_gallery/accounts/routers.py
@@ -0,0 +1,128 @@
+from fastapi import APIRouter, HTTPException, status
+from fastapi.params import Depends
+from pydantic import PositiveInt
+from passlib.hash import pbkdf2_sha256 as plh
+import logging
+
+from latte_gallery.accounts.schemas import (
+    AccountCreateSchema,
+    AccountPasswordUpdateSchema,
+    AccountRegisterSchema,
+    AccountSchema,
+    AccountUpdateSchema,
+    Role,
+)
+from latte_gallery.core.dependencies import AccountServiceDep, SessionDep
+from latte_gallery.core.schemas import Page, PageNumber, PageSize
+from latte_gallery.security.dependencies import AuthenticatedAccount, AuthorizedAccount
+from latte_gallery.security.permissions import Anonymous, Authenticated, IsAdmin
+from passlib.hash import pbkdf2_sha256 as pas_hash
+
+accounts_router = APIRouter(prefix="/accounts", tags=["Аккаунты"])
+
+logger = logging.getLogger(__name__)
+
+
+@accounts_router.post(
+    "/register",
+    summary="Регистрация нового аккаунта",
+    status_code=status.HTTP_201_CREATED,
+    dependencies=[Depends(AuthorizedAccount(Anonymous()))],
+)
+async def register_account(
+    body: AccountRegisterSchema, account_service: AccountServiceDep, session: SessionDep
+) -> AccountSchema:
+    pas = pas_hash.hash(body.password)
+    logger.info("Token is: ", pas)
+    account = await account_service.create(
+        AccountCreateSchema(
+            login=body.login,
+            password=pas,
+            name=body.name,
+            role=Role.USER,
+        ),
+        session,
+    )
+
+    return AccountSchema.model_validate(account)
+
+
+@accounts_router.post(
+    "",
+    summary="Создать новый аккаунт",
+    status_code=status.HTTP_201_CREATED,
+    dependencies=[Depends(AuthorizedAccount(IsAdmin()))],
+)
+async def create_account(
+    body: AccountCreateSchema,
+    current_user: AuthenticatedAccount,
+    account_service: AccountServiceDep,
+    session: SessionDep,
+) -> AccountSchema:
+    assert current_user is not None
+
+    if (current_user.role == Role.MAIN_ADMIN and body.role == Role.MAIN_ADMIN) or (
+        current_user.role == Role.ADMIN and body.role in {Role.ADMIN, Role.MAIN_ADMIN}
+    ):
+        raise HTTPException(status.HTTP_403_FORBIDDEN)
+
+    body.password = plh.hash(body.password)
+
+    account = await account_service.create(body, session)
+
+    return AccountSchema.model_validate(account)
+
+
+@accounts_router.get(
+    "/my",
+    summary="Получение данных своего аккаунта",
+    dependencies=[Depends(AuthorizedAccount(Authenticated()))],
+)
+async def get_my_account(account: AuthenticatedAccount) -> AccountSchema:
+    return AccountSchema.model_validate(account)
+
+
+@accounts_router.get("/{id}", summary="Получение аккаунт по идентификатору")
+async def get_account_by_id(id: PositiveInt, account_service: AccountServiceDep, session: SessionDep) -> AccountSchema:
+    return await account_service.find_by_id(id, session)
+
+
+@accounts_router.get("", summary="Получить список всех аккаунтов")
+async def get_all_accounts(
+    account_service: AccountServiceDep, session: SessionDep, page: PageNumber = 0, size: PageSize = 10
+) -> Page[AccountSchema]:
+    return await account_service.find_all(page, size, session)
+
+
+@accounts_router.put("/my", summary="Обновление данных своего аккаунта")
+async def update_my_account(body: AccountUpdateSchema) -> AccountSchema:
+    return AccountSchema(
+        id=1,
+        login="user1",
+        name="Вася Пупкин",
+        role=Role.USER,
+    )
+
+
+@accounts_router.put("/my/password", summary="Обновить пароль своего аккаунта")
+async def update_my_account_password(
+    body: AccountPasswordUpdateSchema,
+) -> AccountSchema:
+    return AccountSchema(
+        id=1,
+        login="user1",
+        name="Вася Пупкин",
+        role=Role.USER,
+    )
+
+
+@accounts_router.put("/{id}", summary="Обновить аккаунт по идентификатору")
+async def update_account_by_id(
+    id: PositiveInt, body: AccountUpdateSchema
+) -> AccountSchema:
+    return AccountSchema(
+        id=1,
+        login="user1",
+        name="Вася Пупкин",
+        role=Role.USER,
+    )
---
+++ latte_gallery/security/dependencies.py
@@ -0,0 +1,72 @@
+import jwt
+import datetime
+from datetime import timezone, timedelta
+from typing import Annotated
+from dotenv import load_dotenv
+
+from fastapi import Depends, status
+from fastapi.exceptions import HTTPException
+from fastapi.security.http import HTTPBasic, HTTPBasicCredentials
+from passlib.hash import pbkdf2_sha256
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from latte_gallery.accounts.models import Account
+from latte_gallery.core.dependencies import AccountServiceDep, SessionDep
+from latte_gallery.security.permissions import BasePermission
+from latte_gallery.accounts.repository import AccountRepository
+from latte_gallery.core.schemas import  Token
+
+
+
+SecuritySchema = HTTPBasic(auto_error=False)
+cred = Annotated[HTTPBasicCredentials | None, Depends(SecuritySchema)]
+load_dotenv()
+
+async def login_for_access_token(self, login: str, password: str, session: AsyncSession) -> Token:
+    account = await self._repository.find_by_login(login, session)
+    if account is None:
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED)
+    elif pbkdf2_sha256.verify(password, account.password):
+        raise HTTPException(status.HTTP_403_FORBIDDEN)
+    access_token_expires = timedelta(minutes=30)
+    access_token = await create_access_token(
+        data={"login": login, "password": password}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
+
+
+async def authenticate_user(
+    credentials: cred,
+    account_service: AccountServiceDep,
+    session: SessionDep,
+):
+    if credentials is None:
+        return None
+
+    token = await account_service.login_for_access_token(credentials.username, credentials.password, session)
+    user_data = jwt.decode(token, TOKEN_SECRET, algorithms=["HS256"])
+    account = await AccountRepository.find_by_login(user_data["login"], session)
+    return account
+
+
+async def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, TOKEN_SECRET, algorithm="HS256")
+    return encoded_jwt
+
+
+AuthenticatedAccount = Annotated[Account | None, Depends(authenticate_user)]
+
+
+class AuthorizedAccount:
+    def __init__(self, permission: BasePermission):
+        self._permission = permission
+
+    def __call__(self, account: AuthenticatedAccount):
+        if not self._permission.check_permission(account):
+            raise HTTPException(status.HTTP_403_FORBIDDEN)
